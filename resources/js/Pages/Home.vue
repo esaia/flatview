6eeb07673a4e70a23cc3c37cc1e0065b04556b31
Esaia <esaiagafrindashvili@gmail.com>
2026-06-09 21:20:03 +0400

    init

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+//
+</script>
+
+<template>
+    <div class="min-h-screen bg-gray-50 flex items-center justify-center">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-gray-900">Interactive Real Estate</h1>
+            <p class="mt-4 text-lg text-gray-600">Powered by Laravel, Inertia.js, Vue 3 & Tailwind CSS</p>
+        </div>
+    </div>
+</template>
